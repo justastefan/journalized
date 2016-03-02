79c1588ba7346ba44351142f553db503d5df444a
justastefan <stefanmi@hotmail.com>
2016-03-02 22:38:53 +0100

    fixed collapse nabber hack - works after login

--- app/components/main-navigation.js
+++ app/components/main-navigation.js
@@ -17,18 +17,37 @@ export default Ember.Component.extend({
     };
     return result;
   }),
-  didInsertElement: function() {
-    // Bootstrap collapsible navigation bar
-    Ember.$('.nav li a').on('click', function(){
-      Ember.run(function(){
-        var toggle = Ember.$('.navbar-toggle');
-        // Only click if toggle is visible and ...
-        if (toggle && toggle.is(':visible') &&
-            // ... menu item is not a dropdown toggle.
-            !Ember.$(this).hasClass('dropdown-toggle')) {
-              toggle.trigger("click");
-        }
-      });
-    });
-  }
+  /**
+   * Refresh when login changes - due to hidden buttons
+   */
+  navbarCollapseHack: Ember.observer('authManager.isAuthenticated', function() {
+    Ember.run.later(function(){
+      Ember.$('.nav li a').on('click', function(){
+        Ember.run.next(function(){
+          var toggle = Ember.$('.navbar-toggle');
+          // Only click if toggle is visible and ...
+          if (toggle && toggle.is(':visible') &&
+              // ... menu item is not a dropdown toggle.
+              !Ember.$(this).hasClass('dropdown-toggle')) {
+                toggle.trigger("click");
+          }
+        });
+      })
+    }, 300);
+  }).on('didInsertElement')
+
+  // didInsertElement: function() {
+  //   // Bootstrap collapsible navigation bar
+  //   Ember.$('.nav li a').on('click', function(){
+  //     Ember.run(function(){
+  //       var toggle = Ember.$('.navbar-toggle');
+  //       // Only click if toggle is visible and ...
+  //       if (toggle && toggle.is(':visible') &&
+  //           // ... menu item is not a dropdown toggle.
+  //           !Ember.$(this).hasClass('dropdown-toggle')) {
+  //             toggle.trigger("click");
+  //       }
+  //     });
+  //   });
+  // }
 });
